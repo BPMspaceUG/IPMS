ac88bb9c4308f165467d7d249307889e3e1b0704
Rob Kuhlig <rob@bpmspace.org>
2016-08-09 14:30:57 +0200

    new version of GenerateFile.php

--- modules/GenerateFile.php
+++ modules/GenerateFile.php
@@ -17,21 +17,11 @@
 	
 	$array_1 = $result->fetch_all(MYSQLI_ASSOC);
 	
-	$output_index = "// ---------------------------------- index.php starts here --------------------------------------------\n\n";
-	
-	$output_index .= "<?php\n";
-		$output_index .= "// uncomment at least one line!";
-		foreach($array_1 as $value){
-							$output_index .= "//header(\"Location: " . $value['TABLE_NAME'] .".php\");\n";
-			}
-	$output_index .= "?>\n\n";
-	
-	$output_index .= "// ---------------------------------- index.php ends here --------------------------------------------\n\n";
-	
-	$output_header = "// ---------------------------------- _header.inc.php starts here --------------------------------------------\n\n";
-	
+
+	$output_header = "<!DOCTYPE html>\n";
+	$output_header .= "<!-- header starts here -->\n\n";
 	$output_header .= "<?php\n";
-	$output_header .= "\t// uncomment if you want to use LIAM for identity and access management\n";
+	$output_header .= "\t// comment if you do NOT want to use LIAM for identity and access management\n";
 	$output_header .= "\t\tinclude_once '../phpSecureLogin/includes/db_connect.inc.php';\n";
 	$output_header .= "\t\tinclude_once '../phpSecureLogin/includes/functions.inc.php';\n";
 	$output_header .= "\n\tsec_session_start();\n\n";
@@ -42,20 +32,35 @@
 	$output_header .= "\telse {\n";
     $output_header .= "\t\t\$logged = 'in';\n";
 	$output_header .= "\t}\n";
+	$output_header .= "\t// */ end liam header \n";
 	$output_header .= "?>\n";
 	
 	
-	
-	$output_header .= "// ---------------------------------- _header.inc.php ends here --------------------------------------------\n\n";
-	
-	$output_footer = "// ---------------------------------- _footer.inc.php starts here --------------------------------------------\n\n";
-	
-	
-		
-	$output_footer .= "// ---------------------------------- _footer.inc.php ends here --------------------------------------------\n\n";
-	
+	$output_header .= "<html xmlns=\"http://www.w3.org/1999/xhtml\" ng-app=\""."$db_name"."App\">\n";
+	$output_header .= "<head>\n";
+	$output_header .= "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n";
+	$output_header .= "<title>BPMspace "."$db_name"."</title>\n";
+	$output_header .= "<!-- CSS -->\n";
+	$output_header .= "<link rel=\"stylesheet\" href=\"../css/bootstrap.min.css\" media=\"screen\" />\n";
+	$output_header .= "<link rel=\"stylesheet\" href=\"../css/font-awesome.min.css\" />\n";
+	$output_header .= "<link rel=\"stylesheet\" href=\"../css/fuelux.min.css\" />\n";
+	$output_header .= "<link rel=\"stylesheet\" href=\"../css/xeditable.css\" />\n";
 
-	$output_RequestHandler = "// ---------------------------------- Request Handler starts here --------------------------------------------\n\n";
+	$output_header .= "<style>\n";
+	$output_header .= "#bpm-liam-header { margin-top: -20px; margin-bottom: 10px; padding-right: 50px;}\n";
+	$output_header .= "#bpm-logo-care { position:relativ;	z-index: 10;	margin-right: -20px;}\n";
+	$output_header .= "#bpm-logo  { position:relativ; margin-bottom: 20px;}\n";
+	$output_header .= "#bpm-menu {margin-right: 20px; margin-left: 20px; margin-bottom: 10px;}\n";
+	$output_header .= "#bpm-content {margin-right: 20px; margin-left: 20px; margin-bottom: 10px;}\n";
+	$output_header .= "#bpm-footer {margin-right: 10px; margin-left: 10px; margin-bottom: 10px;}\n";
+	$output_header .= "</style>\n";
+
+	$output_header .= "<!-- <link rel=\"stylesheet\" href=\"css/"."$db_name".".css\" /> -->\n";
+	$output_header .= "</head>\n";
+
+	$output_header .= "<!--  header ends here -->\n\n";
+	
+	$output_RequestHandler = "<!--  Request Handler starts here  -->\n\n";
 	
 	$query = "SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '$table_name' AND TABLE_SCHEMA = '$db_name'";
 	
@@ -65,17 +70,17 @@
 	$array_2 = $result->fetch_all(MYSQLI_ASSOC);
 	
 	
-	$output_RequestHandler .= "//Table \"" . $table_name . "\" in database " . $db_name . " has ". $result->num_rows . " coloumns. \n";
+	$output_RequestHandler .= "<!-- Table \"" . $table_name . "\" in database " . $db_name . " has ". $result->num_rows . " coloumns. -->\n";
 	
-	$output_RequestHandler .= "//\tCOLUMN_NAME[ORDINAL_POSITION]:\tvalue\tCOLUMN_TYPE\tCOLUMN_KEY\n";
+	$output_RequestHandler .= "<!-- \tCOLUMN_NAME[ORDINAL_POSITION]:\tvalue\tCOLUMN_TYPE\tCOLUMN_KEY-->\n";
 	foreach($array_2 as $value){
-		$output_RequestHandler .= "//\tCOLUMN_NAME[" . $value['ORDINAL_POSITION'] . "]:\t\t" . $value['COLUMN_NAME']. "\t\t" .$value['COLUMN_TYPE']. "\t\t" .$value['COLUMN_KEY']."\n";
+		$output_RequestHandler .= "<!-- \tCOLUMN_NAME[" . $value['ORDINAL_POSITION'] . "]:\t\t" . $value['COLUMN_NAME']. "\t\t" .$value['COLUMN_TYPE']. "\t\t" .$value['COLUMN_KEY']."-->\n";
 	}
 	$output_RequestHandler .= "\n\r\n\r";
 	//print_r ($array_2);
 	
 	if ($array_2[0]['COLUMN_KEY'] != "PRI"){
-	$output_RequestHandler .= "WARNING - FIRST Column not PRIMARY KEY \n";
+	$output_RequestHandler .= "<!--WARNING - FIRST Column not PRIMARY KEY -->\n";
 	}
 	
 	
@@ -139,16 +144,133 @@
 	$output_RequestHandler .= "\t\t\tbreak;\n";
     $output_RequestHandler .= "\t\t}\n";
     $output_RequestHandler .= "\t}\n";
+    $output_RequestHandler .= "}\n";
 	
-	$output_RequestHandler .= "\n\n?>";
+	$output_RequestHandler .= "\n?>\n";
 	
-	$output_RequestHandler .= "\n// ---------------------------------- Request Handler ends here --------------------------------------------\n\n";
+	$output_RequestHandler .= "<!-- Request Handler ends here  -->\n\n";
+		
+	$output_menu = "<!--  body menu starts here -->\n\n";
 	
+	$output_menu .= "<body ng-controller=\""."$db_name"."Ctrl\">\n";
+	$output_menu .= "\t<div class=\"container\">\n";
+	$output_menu .= "\t\t<div class=\"row\">\n";
 	
-	echo $output_index;
+	$output_menu .= "\t\t<div  class=\"row text-right\">\n";
+	$output_menu .= "\t\t\t<div class=\"col-md-12\">\n";
+    $output_menu .= "\t\t\t<a href='#' id=\"bpm-logo-care\" class=\"btn collapsed\" data-toggle=\"collapse\" data-target=\"#bpm-logo, #bpm-liam-header\"><i class=\"fa fa-caret-square-o-down\"></i></a>\n";
+    $output_menu .= "\t\t\t</div>\n";
+	
+	$output_menu .= "\t\t\t<div class=\"col-md-12 collapse in text-right\" id=\"bpm-liam-header\">\n";
+	$output_menu .= "\t\t\t\t<?php\n";
+	$output_menu .= "\t\t\t\t\t// comment if you do NOT want to use LIAM for identity and access management\n";
+	$output_menu .= "\t\t\t\t\tinclude_once '../_header_LIAM.inc.php'\n";
+	$output_menu .= "\t\t\t\t\t// end liam header\n";
+	$output_menu .= "\t\t\t\t?>\n";
+	$output_menu .= "\n\t\t\t</div>\n";
+	$output_menu .= "\t\t</div>\n";
+	$output_menu .= "\t</div>\n";
+	
+
+	
+	$output_menu .= "\t\t<div class=\"col-md-12 collapse in\" id=\"bpm-logo\">\n";
+	$output_menu .= "\t\t\t<div class=\"col-md-6 \"><img class=\"img-rounded\" src=\"http://dummyimage.com/100x100/ff0000/FFF.png&text=your+logo\" alt=\"your logo\" /></div>\n";
+	$output_menu .= "\t\t\t<div class=\"col-md-6 \"><img class=\"pull-right img-rounded\" src=\"http://dummyimage.com/200x100/0000ff/FFF.png&text="."$db_name"."\" alt=\""."$db_name"."\" /></div>\n";
+    $output_menu .= "\t\t</div>\n";
+	$output_menu .= "\t</div>\n";
+	$output_menu .= "\n";
+	
+
+	$output_menu .= "<nav class=\"navbar navbar-default bg-faded\">\n";
+	$output_menu .= "\t<div class=\"container\">\n";
+	$output_menu .= "\t\t<ul class=\"nav nav-tabs\" id=\"bpm-menu\">\n";
+	
+	foreach($array_1 as $value){
+		$output_menu .= "\t\t\t<li><a title=\"".$value['TABLE_NAME']."\" href=\"#".$value['TABLE_NAME']."\" data-toggle=\"tab\"><i class=\"fa fa-circle-o\"></i> ".$value['TABLE_NAME']."</a></li>\n";
+	}
+	
+	$output_menu .= "\n";
+	$output_menu .= "\t\t</ul>\n";
+	$output_menu .= "\t</div>\n";
+	$output_menu .= "</nav>\n";
+
+	$output_menu .= "\n";
+	$output_menu .= "\n";
+	$output_menu .= "\n\n";
+	
+	$output_menu .= "<!--  body menu starts here -->\n\n";
+	
+	$output_content = "<!--  body content starts here -->\n\n";
+	
+	$output_content .= "<div class=\"container\">\n";
+	$output_content .= "\t\t<div class=\"row\">\n";
+	
+	$output_content .="\t\t<div class=\"col-md-12 tab-content\" ng-controller=\"navCtrl\" id=\"bpm-content\">\n";
+		
+	$i = 0;
+	foreach($array_1 as $value){
+				$output_content .= "\t\t\t\t<div class=\"tab-pane";
+				if ($i == 0) {$output_content .= " active";}
+				$output_content .= "\" id=\"".$value['TABLE_NAME']."\">\n";
+				$output_content .= "\t\t\t\t<h2>".$value['TABLE_NAME']."</h2>\n";
+				$output_content .= "\t\t\t\t".$value['TABLE_NAME']."</div>\n";
+				$i++;
+				}
+				
+	$output_content .= "\t\t</div>\n";
+	$output_content .= "\t</div>\n";
+	$output_content .= "</div>\n";
+	
+	$output_content .= "<!--  body content ends here -->\n\n";
+	
+
+	$output_footer = "<!--  footer starts here -->\n\n";
+
+	$output_footer .= "<div class=\"container\">\n";
+	$output_footer .= "\t<div class=\"row well\" id=\"bpm-footer\">\n";
+	$output_footer .= "\t\t\t<div class=\"col-md-3\">BPMspace "."$db_name"." using</div>\n";
+	$output_footer .= "\t\t\t<small><div class=\"col-md-9\">\n";
+	$output_footer .= "\t\t\t\t<ul class=\"list-inline\">\n";
+	$output_footer .= "\t\t\t\t\t<li><a href=\"http://getbootstrap.com/\" target=\"_blank\">Bootstrap</a></li>\n";
+	$output_footer .= "\t\t\t\t\t<li><a href=\"https://jquery.com/\" target=\"_blank\">jQuery</a></li>\n";
+	$output_footer .= "\t\t\t\t\t<li><a href=\"https://angularjs.org/\" target=\"_blank\">AngularJS</a></li>\n";
+	$output_footer .= "\t\t\t\t\t<li><a href=\"http://php.net/\" target=\"_blank\">PHP</a></li>\n";
+	$output_footer .= "\t\t\t\t\t<li><a href=\"http://getfuelux.com/\" target=\"_blank\">FuelUX</a></li>\n";
+	$output_footer .= "\t\t\t\t\t<li><a href=\"https://angular-ui.github.io/\" target=\"_blank\">AngularUI</a></li>\n";
+	$output_footer .= "\t\t\t\t\t<li><a href=\"https://www.tinymce.com/\" target=\"_blank\">TinyMCE</a></li>\n";
+	$output_footer .= "\t\t\t\t\t<li><a href=\"https://vitalets.github.io/x-editable/\" target=\"_blank\">X-editable</a></li>\n";
+	$output_footer .= "\t\t\t\t\t<li><a href=\"https://github.com/peredurabefrog/phpSecureLogin\" target=\"_blank\">phpSecureLogin</a></li>\n";
+	$output_footer .= "\t\t\t\t</ul>\n";
+	$output_footer .= "\t\t</div><small>\n";
+	$output_footer .= "\t</div>\n";
+	$output_footer .= "</div>\n";
+
+	
+	$output_footer .= "<!-- JS -->\n";
+	$output_footer .= "<script type=\"text/javascript\" src=\"../js/angular.min.js\"></script>\n";
+	$output_footer .= "<script type=\"text/javascript\" src=\"../js/angular-sanitize.min.js\"></script>\n";
+	$output_footer .= "<script type=\"text/javascript\" src=\"../js/ui-bootstrap-1.3.1.min.js\"></script>\n";
+	$output_footer .= "<script type=\"text/javascript\" src=\"../js/ui-bootstrap-tpls-1.3.1.min.js\"></script>\n";
+	$output_footer .= "<script type=\"text/javascript\" src=\"../js/jquery-2.1.4.min.js\"></script>\n";
+	$output_footer .= "<script type=\"text/javascript\" src=\"../js/tinymce.min.js\"></script>\n";
+	$output_footer .= "<script type=\"text/javascript\" src=\"../js/tinymceng.js\"></script>\n";
+	$output_footer .= "<script type=\"text/javascript\" src=\"../js/bootstrap.min.js\"></script>\n";
+	$output_footer .= "<script type=\"text/javascript\" src=\"../js/xeditable.min.js\"></script>\n";
+		
+	$output_footer .= "<!-- <script type=\"text/javascript\" src=\"js/"."$db_name".".js\"></script> -->\n";
+
+
+	$output_footer .= "</body>\n";
+	$output_footer .= "</html>\n";
+	$output_footer .= "\n\n";
+
+	$output_footer .= "<!--  footer ends here -->\n\n";
+		
 	echo $output_header;
+	//echo $output_RequestHandler;
+	echo $output_menu;
+	echo $output_content;
 	echo $output_footer;
-	echo $output_RequestHandler;
 	
 
 ?>
